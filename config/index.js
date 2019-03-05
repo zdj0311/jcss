07e7895acfe54a28cad8272eb09364127a023db8
zhengdj <546804672@qq.com>
2019-03-05 16:51:52 +0800

    update .gitignore

--- config/index.js
+++ config/index.js
@@ -12,7 +12,7 @@ module.exports = {
     assetsPublicPath: '/',
     proxyTable: {
       '/search': {
-        target: 'http://192.168.210.70:8080', //目标接口域名
+        target: 'http://192.168.210.50:8080', //目标接口域名
 //      target: 'http://192.168.210.70:8080', //目标接口域名
         changeOrigin: true, //是否跨域
       }
