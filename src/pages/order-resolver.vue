f9a45df61d460b0c314c78ba6e2a51a61262899d
zhengdj <546804672@qq.com>
2019-03-12 14:20:48 +0800

    创建工单

--- src/pages/order-resolver.vue
+++ src/pages/order-resolver.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="order-resolver">
-      <order-form :readonlyList="readonlyList" :create=false :fData="loadData.billData" v-if="loadData.billData"></order-form>
+      <order-form :readonlyList="readonlyList" :mode="'edit'"></order-form>
     </div>
 </template>
 
@@ -8,28 +8,18 @@
 import {getCustomerOrgDic,getBtDic,getCustomerDic,getUrgencyDic,getProjectDic,getProjectSubDic,getAssetType,getAssetsList,loadWorkflow,updateWorkflow,upload}
  from 'controller/order-create'
 import orderForm from 'components/order-form'
-import tool from 'utils/tool'
 export default {
   components: {orderForm},
   data() {
     return {
-        readonlyList:[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15],
+        existList:[2],
+        readonlyList:[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16],
         loadData: {},
     };
   },
   methods: {
-    /**
-     * @desc 工作流初始化
-    */
-    loadWorkflow(){
-      loadWorkflow.bind(this)(tool.getQueryString('id')?tool.getQueryString('id'):276).then(res=>{
-        this.loadData = res;
-      }) 
-    }
   },
-  created(){
-    this.loadWorkflow();
-    
+  created(){  
   },
   mounted() {
   }
